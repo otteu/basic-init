2e73452e7bac9c57a49dc0832bade433b333f9f2
otteu <ehdtjsm@gmail.com>
2023-11-05 01:26:05 +0900

    제목 : 파라미터 값 검증 추가

--- api/src/main/java/dev/example/api/common/error/ErrorCode.java
+++ api/src/main/java/dev/example/api/common/error/ErrorCode.java
@@ -13,8 +13,10 @@ public enum ErrorCode implements ErrorCodeIfs {
 
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, "서버에러"),
 
-    NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR.value(), 512, "Null point")
+    NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR.value(), 512, "Null point"),
 
+    // 파라미터 valid 검증
+    PARAMETER_NO_VALIDATION(HttpStatus.BAD_REQUEST.value(), 400, "파라미터 값 검증 실패")
     ;
 
     private final Integer httpStatusCode;
